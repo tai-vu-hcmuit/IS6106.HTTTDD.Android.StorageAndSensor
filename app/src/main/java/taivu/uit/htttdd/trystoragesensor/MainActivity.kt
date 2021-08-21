39ffe7577b9915f2f0a9632acbaf7509432dbfb2
vcttai <vcttai@gmail.com>
2021-08-21 14:11:47 +0700

    Save Accelerator info to Shared Preferences Storage.

--- app/src/main/java/taivu/uit/htttdd/trystoragesensor/MainActivity.kt
+++ app/src/main/java/taivu/uit/htttdd/trystoragesensor/MainActivity.kt
@@ -1,58 +1,106 @@
 package taivu.uit.htttdd.trystoragesensor
 
+import android.content.Context
 import android.content.SharedPreferences
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import java.math.RoundingMode
 
-class MainActivity : AppCompatActivity() {
-    var rotatedLeftCount: Int = 0;
-    var rotatedRightCount: Int = 0;
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    var accelerateX: Float = 0f;
+    var accelerateY: Float = 0f;
+    var accelerateZ: Float = 0f;
+    private lateinit var mSensorManager : SensorManager
+    private var mAccelerator: Sensor ?= null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Get count number on Storage
-        val counts = this.getCountFromStorage();
-        this.rotatedLeftCount = counts[0];
-        this.rotatedRightCount = counts[1];
+        val lastValues = this.getAcceleraterValuesFromStorage();
+        this.viewLastValues(lastValues[0], lastValues[1], lastValues[2]);
 
-        this.viewCount(this.rotatedLeftCount, this.rotatedRightCount);
+        this.mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        this.mAccelerator = this.mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 
-    private fun viewCount(leftCount: Int, rightCount: Int) {
-        val txtLeftValue = findViewById<TextView>(R.id.rotate_left_value);
-        val txtRightValue = findViewById<TextView>(R.id.rotate_right_value);
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event == null) {
+            return
+        }
 
-        txtLeftValue.text = leftCount.toString();
-        txtRightValue.text = rightCount.toString();
+        if(event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val txtX = findViewById<TextView>(R.id.current_x);
+            val txtY = findViewById<TextView>(R.id.current_y);
+            val txtZ = findViewById<TextView>(R.id.current_z);
+
+            this.accelerateX = event.values[0].toBigDecimal().setScale(3, RoundingMode.FLOOR).toFloat();
+            this.accelerateY = event.values[1].toBigDecimal().setScale(3, RoundingMode.FLOOR).toFloat();
+            this.accelerateZ = event.values[2].toBigDecimal().setScale(3, RoundingMode.FLOOR).toFloat();
+
+            txtX.text = this.accelerateX.toString();
+            txtY.text = this.accelerateY.toString();
+            txtZ.text = this.accelerateZ.toString();
+        }
     }
 
-    private fun getCountFromStorage(): Array<Int> {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.e("Test onAccuracyChanged", "Accuracy changed")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        this.mSensorManager.registerListener(this, this.mAccelerator, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        this.mSensorManager.unregisterListener(this)
+    }
+
+    private fun viewLastValues(x: Float, y: Float, z: Float) {
+        val txtX = findViewById<TextView>(R.id.accelerate_x);
+        val txtY = findViewById<TextView>(R.id.accelerate_y);
+        val txtZ = findViewById<TextView>(R.id.accelerate_z);
+
+        txtX.text = x.toString();
+        txtY.text = y.toString();
+        txtZ.text = z.toString();
+    }
+
+    private fun getAcceleraterValuesFromStorage(): Array<Float> {
         val ds: SharedPreferences = this.getSharedPreferences("rotateDB", MODE_PRIVATE);
 
-        val leftCount = ds.getInt("rotatedLeftCount", 0);
-        val rightCount = ds.getInt("rotatedRightCount", 0);
+        val x = ds.getFloat("accelerateX", 0f);
+        val y = ds.getFloat("accelerateY", 0f);
+        val z = ds.getFloat("accelerateZ", 0f);
 
-        return arrayOf(leftCount, rightCount);
+        return arrayOf(x, y, z);
     }
 
-    private fun saveCountToStorage(leftCount: Int, rightCount: Int) {
+    private fun saveCountToStorage(x: Float, y: Float, z: Float) {
         val ds: SharedPreferences.Editor = this.getSharedPreferences("rotateDB", MODE_PRIVATE).edit();
 
-        ds.putInt("rotatedLeftCount", leftCount);
-        ds.putInt("rotatedRightCount", rightCount);
+        ds.putFloat("accelerateX", x);
+        ds.putFloat("accelerateY", y);
+        ds.putFloat("accelerateZ", z);
         ds.apply();
     }
 
-    fun clearData(view: View) {
-        this.rotatedLeftCount += 5;
-        this.rotatedRightCount += 3;
-
-        this.saveCountToStorage(this.rotatedLeftCount, this.rotatedRightCount);
-        this.viewCount(this.rotatedLeftCount, this.rotatedRightCount);
+    fun saveDataToStorage(view: View) {
+        this.saveCountToStorage(this.accelerateX, this.accelerateY, this.accelerateZ);
+        this.viewLastValues(this.accelerateX, this.accelerateY, this.accelerateZ);
     }
 
 
